7cbfb62b3ae655f3bb69bfa6adb993f1c9fbf3c7
jyoti <riamishra639@gmail.com>
2024-07-12 14:18:18 +0530

    https://github.com/jyotimishra6789/travel

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
 import React from 'react';
+import {CssBaseline, Grid} from '@material-ui/core';
+import Header from './components/Header/Header';
+import List from './components/List/List';
+import Map from './components/Map/Map';
+
 const App=() => {
     return(
         
